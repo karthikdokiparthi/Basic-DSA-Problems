56d64e8d09f2a985407bf47111c5d7d21f48b8e2
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-10-14 07:46:03 +0530

    Added program to check if a number is positive, negative, or zero

--- Basci-Problems/PositiveAndNegative.java
+++ Basci-Problems/PositiveAndNegative.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class PositiveAndNegative {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println("The Number is Postive");
+        } else if (num < 0) {
+            System.out.println("The Number is Negative");
+        } else {
+            System.out.println("Zero ");
+        }
     }
 }
