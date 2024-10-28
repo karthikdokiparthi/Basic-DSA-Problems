8d6f597f024d27437c7b1e2bd186e2fb4e11961e
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-10-28 15:18:30 +0530

    Add recursive function to calculate the length of a string

--- Recursion/LengthofString.java
+++ Recursion/LengthofString.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class LengthofString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Scentance or Name : ");
+        System.out.print("Enter a Sentence or Name : ");
         String str = sc.next();
-        int res = countString(str, 0);
+        int res = countString(str);
+        // int res = countString(str,0);
         System.out.println("length of the string : " + res);
     }
 
-    static int countString(String str, int count) {
-        if (count == str.length()) {
-            return count;
+    static int countString(String str) {
+        if (str == "") {
+            return 0;
         }
-        return countString(str, count + 1);
+        return 1 + countString(str.substring(1));
     }
+
+    // static int countString(String str, int count) {
+    // if (count == str.length()) {
+    // return count;
+    // }
+    // return countString(str, count + 1);
+    // }
 }
