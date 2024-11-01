7d8d44438706ec1dfc8a3e1869122f8b6efb024d
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-11-01 08:17:58 +0530

    Implement array sorting in ascending order using nested loops

---
+++ Related to Arrays/SortanArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SortanArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the lenght of Array : ");
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter a number : ");
+            arr[i] = sc.nextInt();
+        }
+        int temp = 0;
+        // Sort the array in ascending order
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
