dcab98ba73f37ebc234d48ca27d823e938a2623a
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-01-02 07:39:24 +0530

    Implemented Inverted Right-Angled Triangle

---
+++ Patterns/InvertedRightAngledTriangle.java
@@ -0,0 +1,11 @@
+public class InvertedRightAngledTriangle {
+    public static void main(String[] args) {
+        int num = 5;
+        for (int i = num; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
